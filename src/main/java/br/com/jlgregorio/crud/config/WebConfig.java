99fa715b5987007f3cbe7b4f0559b08fcb5fb374
Jorge Luís Gregório <jlgregorio81@gmail.com>
2022-05-05 09:54:58 -0300

    Introduction to Migrations

--- src/main/java/br/com/jlgregorio/crud/config/WebConfig.java
+++ src/main/java/br/com/jlgregorio/crud/config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.ContentNegotiationConfi
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.print.attribute.standard.Media;
 import java.util.List;
 
 @Configuration
---
+++ src/main/resources/db/migration/V1.1__insert_into_table_person.sql
@@ -0,0 +1,6 @@
+insert into person (first_name, last_name, profession, gender)
+values
+   ('Jorge', 'Gregório', 'Professor', 'Masculino'),
+   ('Patrícia', 'Gregório', 'Professora', 'Feminino'),
+   ('Emanuel', 'Gregório', 'Estudante', 'Masculino'),
+   ('Luke', 'Skywalker', 'Cavaleiro Jedi', 'Masculino');
